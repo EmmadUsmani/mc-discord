1afbe9e365a8aed30d5e5c80c8a96bd17cd7db78
Emmad Usmani <emmadusmani@berkeley.edu>
2021-09-20 12:04:01 -0700

    feat: :tada: create test plugin
    Created a test plugin following a video tutorial (https://www.youtube.com/watch?v=GK4aQzxQSoQ). This is not the actual functionality we intend to implement

--- src/main/java/com/emmad/mcdiscord/Main.java
+++ src/main/java/com/emmad/mcdiscord/Main.java
@@ -7,7 +7,9 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        System.out.println("Hello World!");
 
+        getServer().getPluginManager().registerEvents(new BreakBlock(), this);
     }
 
     @Override
