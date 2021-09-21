4503bd72693c14c85551ddd9ece795f2c6bbd396
Emmad Usmani <emmadausmani@gmail.com>
2021-09-21 08:54:36 -0700

    feat(coordinates): :sparkles: add support for manual coordinate entry in save-coord command
    Added support for manually supplying 3 coordinates after the name in the 'save-coord' command (renamed from 'savecoord'). Appropriate error handling was implemented.

--- src/main/java/com/emmad/mcdiscord/MCDiscordCommandExecutor.java
+++ src/main/java/com/emmad/mcdiscord/MCDiscordCommandExecutor.java
@@ -11,12 +11,12 @@ public class MCDiscordCommandExecutor implements CommandExecutor {
 
     public MCDiscordCommandExecutor(MCDiscord plugin) {
         this.plugin = plugin;
-        this.plugin.getCommand("savecoord").setExecutor(this);
+        this.plugin.getCommand("save-coord").setExecutor(this);
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("savecoord")) {
+        if (command.getName().equalsIgnoreCase("save-coord")) {
             return this.handleSaveCoord(sender, command, label, args);
         }
         return false;
@@ -31,15 +31,35 @@ public class MCDiscordCommandExecutor implements CommandExecutor {
             sender.sendMessage("Must specify a name for this coordinate.");
             return false;
         }
-        if (args.length > 1) {
+        if (args.length > 1 && args.length < 4) {
+            sender.sendMessage("Must specify all three coordinates (x, y, z).");
+            return false;
+        }
+        if (args.length > 4) {
             sender.sendMessage("Too many arguments.");
             return false;
         }
 
+        double x, y, z;
         String name = args[0];
         Player player = (Player) sender;
-        Location location = player.getLocation();
-        this.plugin.getLogger().info(name + " " + location.getX() + " " + location.getY() + " " + location.getZ());
+
+        if (args.length == 1) {
+            x = player.getLocation().getX();
+            y = player.getLocation().getY();
+            z = player.getLocation().getZ();
+        } else {
+            try {
+                x = Double.parseDouble(args[1]);
+                y = Double.parseDouble(args[2]);
+                z = Double.parseDouble(args[3]);
+            } catch (NumberFormatException e) {
+                sender.sendMessage("Coordinates must be valid decimal numbers.");
+                return false;
+            }
+        }
+
+        this.plugin.getLogger().info(name + " " + x + " " + y + " " + z);
 
         sender.sendMessage("Coordinate saved.");
         return true;
