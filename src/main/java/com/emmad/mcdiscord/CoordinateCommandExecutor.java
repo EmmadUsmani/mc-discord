575684556007616f2e946533ba7ff3396c6821ca
Emmad Usmani <emmadausmani@gmail.com>
2021-09-23 10:55:56 -0700

    style(coordinates): :art: rename MCDiscord to Main and MCDiscordCommandExecutor to CoordinateCommandExecutor
    Renamed these files and did minor refactoring in CoordinateCommandExecutor

---
+++ src/main/java/com/emmad/mcdiscord/CoordinateCommandExecutor.java
@@ -0,0 +1,174 @@
+package com.emmad.mcdiscord;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collection;
+
+public class CoordinateCommandExecutor implements CommandExecutor {
+    private final JavaPlugin plugin;
+    private final CoordinateManager coordinateManager;
+
+    public CoordinateCommandExecutor(Main plugin, CoordinateManager coordinateManager) throws InitializationFailedException {
+        this.plugin = plugin;
+        this.coordinateManager = coordinateManager;
+
+        try {
+            this.plugin.getCommand("save-coord").setExecutor(this);
+            this.plugin.getCommand("delete-coord").setExecutor(this);
+            this.plugin.getCommand("list-coords").setExecutor(this);
+            this.plugin.getCommand("get-coord").setExecutor(this);
+        } catch (NullPointerException e) {
+            throw new InitializationFailedException("NullPointerException while calling setExecutor." +
+                    " Developer, make sure commands are defined in plugin.yml.");
+        }
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("save-coord")) {
+            return handleSaveCoord(sender, command, label, args);
+        }
+        if (command.getName().equalsIgnoreCase("delete-coord")) {
+            return handleDeleteCoord(sender, command, label, args);
+        }
+        if (command.getName().equalsIgnoreCase("list-coords")) {
+            return handleListCoords(sender, command, label, args);
+        }
+        if (command.getName().equalsIgnoreCase("get-coord")) {
+            return handleGetCoord(sender, command, label, args);
+        }
+        return false;
+    }
+
+    private boolean handleSaveCoord(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Command must be run by a player.");
+            return true;
+        }
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Must specify a one word name for this coordinate.");
+            return false;
+        }
+        if (args.length > 1 && args.length < 4) {
+            sender.sendMessage(ChatColor.RED + "Incorrect number of arguments. Make sure your coordinate name " +
+                    "is only one word and that you are specifying either 0 or 3 coordinates (x y z).");
+            return false;
+        }
+        if (args.length > 4) {
+            sender.sendMessage(ChatColor.RED + "Incorrect number of arguments. Make sure your coordinate name " +
+                    "is only one word and that you are specifying either 0 or 3 coordinates (x y z).");
+            return false;
+        }
+
+        Location location;
+        String name = args[0];
+        Player player = (Player) sender;
+
+        if (args.length == 1) {
+            location = player.getLocation();
+        } else {
+            try {
+                double x = Double.parseDouble(args[1]);
+                double y = Double.parseDouble(args[2]);
+                double z = Double.parseDouble(args[3]);
+                location = new Location(player.getWorld(), x, y, z);
+            } catch (NumberFormatException e) {
+                sender.sendMessage(ChatColor.RED + "Coordinates must be valid decimal numbers.");
+                return false;
+            }
+        }
+
+        try {
+            coordinateManager.saveCoordinate(name, player.getName(), location);
+            sender.sendMessage(ChatColor.GREEN + "Coordinate saved.");
+        } catch (CoordinateManager.DuplicateCoordinateException e) {
+            sender.sendMessage(ChatColor.RED + e.getMessage());
+        } catch (DiscordBot.RequestFailedException e) {
+            sender.sendMessage(ChatColor.DARK_RED + "Failed to save coordinate, see server log.");
+            plugin.getLogger().info(e.getMessage());
+        }
+
+        return true;
+    }
+
+    private boolean handleDeleteCoord(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "Command must be run by a player.");
+            return true;
+        }
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Must specify a coordinate name.");
+            return false;
+        }
+        if (args.length > 1) {
+            sender.sendMessage(ChatColor.RED + "Too many arguments.");
+            return false;
+        }
+
+        String name = args[0];
+        try {
+            coordinateManager.deleteCoordinate(name, sender.getName());
+            sender.sendMessage(ChatColor.GREEN + "Coordinate deleted.");
+        } catch (CoordinateManager.CoordinateDoesNotExistException
+                | CoordinateManager.PlayerLacksPermissionException e) {
+            sender.sendMessage(ChatColor.RED + e.getMessage());
+        } catch (DiscordBot.RequestFailedException e) {
+            sender.sendMessage(ChatColor.DARK_RED + "Failed to delete coordinate, see server log.");
+            plugin.getLogger().info(e.getMessage());
+        }
+        return true;
+    }
+
+    private boolean handleListCoords(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length > 0) {
+            sender.sendMessage(ChatColor.RED + "Too many arguments.");
+            return false;
+        }
+
+        Collection<CoordinateManager.Coordinate> coordinates = coordinateManager.getCoordinates();
+        if (coordinates.isEmpty()) {
+            sender.sendMessage("There are no saved coordinates. You can save a coordinate with "
+                    + ChatColor.AQUA + "/save-cord" + ChatColor.RESET + ".");
+        } else {
+            for (CoordinateManager.Coordinate coordinate : coordinates) {
+                sender.sendMessage(coordinate.toMinecraftString());
+            }
+        }
+
+        return true;
+    }
+
+    private boolean handleGetCoord(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage(ChatColor.RED + "Must specify a coordinate name.");
+            return false;
+        }
+        if (args.length > 1) {
+            sender.sendMessage(ChatColor.RED + "Too many arguments.");
+            return false;
+        }
+
+        String name = args[0];
+        try {
+            CoordinateManager.Coordinate coordinate = coordinateManager.getCoordinate(name);
+            sender.sendMessage(coordinate.toMinecraftString());
+        } catch (CoordinateManager.CoordinateDoesNotExistException e) {
+            sender.sendMessage(ChatColor.RED + e.getMessage());
+        }
+
+        return true;
+    }
+
+    public static class InitializationFailedException extends Exception {
+        public InitializationFailedException(String message) {
+            super(message);
+        }
+    }
+}
--- src/main/java/com/emmad/mcdiscord/Main.java
+++ src/main/java/com/emmad/mcdiscord/Main.java
@@ -3,7 +3,7 @@ package com.emmad.mcdiscord;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class MCDiscord extends JavaPlugin {
+public final class Main extends JavaPlugin {
     private DiscordBot discordBot;
 
     @Override
@@ -13,8 +13,7 @@ public final class MCDiscord extends JavaPlugin {
         try {
             this.discordBot = new DiscordBot(this);
             CoordinateManager coordinateManager = new CoordinateManager(this, discordBot);
-            new MCDiscordCommandExecutor(this, coordinateManager);
-
+            new CoordinateCommandExecutor(this, coordinateManager);
         } catch (Exception e) {
             getLogger().info(ChatColor.RED + e.getMessage());
             getLogger().info(ChatColor.DARK_RED + "Initialization failed, disabling plugin.");
